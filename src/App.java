034e61024e5330e4e1a67908eb84910520ff73a9
=Mateus Timm <mateustimm@hotmail.com>
2023-06-04 22:58:31 -0300

    Inicio de projeto
    Leitor CSV

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+public class App{
+    public static void main(String[] args) {
+        try {
+            String arquivo = ".\\teste.csv";
+            Leitor ler = new Leitor();
+            Dado[] dados = ler.importar(arquivo);
+
+            for (int i = 0; i < dados.length; i++) {
+                System.out.println(dados[i].toString());
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
